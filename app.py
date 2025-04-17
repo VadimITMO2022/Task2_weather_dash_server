48395bf17bb4550dab23a039da19171b2dbfb0d2
Vadim Varshavskiy <vadim_varshavskiy@yahoo.com>
2025-04-17 23:31:11 +0300

    minor addition

--- app.py
+++ app.py
@@ -5,9 +5,11 @@ import dash_bootstrap_components as dbc
 
 app = Dash(external_stylesheets=[dbc.themes.DARKLY])
 app.title = "Состояние воздуха в городе"
+# контакт с сервером
 server=app.server
 
-app.layout = create_layout()
+app.layout = create_layout() 
+
 
 register_callbacks(app)
 
