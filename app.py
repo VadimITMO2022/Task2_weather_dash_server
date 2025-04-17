8d6e3bd00716ad599409bfc8f90f0eb3d030a71f
Vadim Varshavskiy <vadim_varshavskiy@yahoo.com>
2025-04-17 18:48:48 +0300

    добавил команду server

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import dash_bootstrap_components as dbc
 
 app = Dash(external_stylesheets=[dbc.themes.DARKLY])
 app.title = "Состояние воздуха в городе"
-#server=app.server
+server=app.server
 
 app.layout = create_layout()
 
